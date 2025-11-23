e38cc6d85ea35bc81b1c79ede2b82aed0dada905
sagevik <rsagevik@gmail.com>
2025-11-23 01:52:01 +0100

    update mango

--- mango/.config/mango/autostart.sh
+++ mango/.config/mango/autostart.sh
@@ -1,5 +1,5 @@
-waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css >/dev/null 2>&1 &
-swaybg -i ~/pix/wallpapers/minimalist_lamp.png >/dev/null 2>&1 &
-nm-applet &
-blueman-applet &
+# waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css >/dev/null 2>&1 &
+# swaybg -i ~/pix/wallpapers/minimalist_lamp.png >/dev/null 2>&1 &
+# nm-applet &
+# blueman-applet &
 
