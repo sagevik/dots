b5cd15f128b3c1eeaf8f53bf45b4cd4a2e9d0152
sagevik <rsagevik@gmail.com>
2025-11-24 20:28:15 +0100

    mango

--- mango/.config/mango/scripts/snip.sh
+++ mango/.config/mango/scripts/snip.sh
@@ -1,1 +1,19 @@
-grim -l 0 -g "$(slurp)" - | wl-copy
+choice=$1
+
+time=$(date +%Y-%m-%d_%H-%M-%S)
+file="/home/rs/pix/screenshots/screenshot_$time.png"
+case $choice in
+  "selection")
+    grim -l 0 -g "$(slurp)" $file
+    notify-send "Screenshot" "$choice saved to $file"
+    ;;
+  "screen")
+    grim -l 0 $file
+    notify-send "Screenshot" "$choice saved to $file"
+    ;;
+  "clipboard")
+    grim -l 0 -g "$(slurp)" - | wl-copy
+    notify-send "Screenshot" "put to $choice"
+    ;;
+
+esac
