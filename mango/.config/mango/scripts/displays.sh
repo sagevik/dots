d717b644f69417d4e20d9d6cb868ec9961593143
sagevik <rsagevik@gmail.com>
2026-01-24 20:17:46 +0100

    color and display setup

--- mango/.config/mango/scripts/displays.sh
+++ mango/.config/mango/scripts/displays.sh
@@ -5,13 +5,13 @@ option=$(printf "38 dual\n38 only\nIntegrated\nManual" | wmenu -i -l 4 -p "Selec
 case $option in
     "38 dual")
         wlr-randr --output eDP-1 --on
-        wlr-randr --output DP-6 --on
-        wlr-randr --output DP-6 --mode 3840x1600 --right-of eDP-1 --output eDP-1 --mode 1920x1200
+        wlr-randr --output DP-5 --on
+        wlr-randr --output DP-5 --mode 3840x1600 --right-of eDP-1 --output eDP-1 --mode 1920x1200
         ;;
     "38 only")
-        wlr-randr --output DP-6 --on
+        wlr-randr --output DP-5 --on
         wlr-randr --output eDP-1 --off
-        wlr-randr --output DP-6 --mode 3840x1600
+        wlr-randr --output DP-5 --mode 3840x1600
         ;;
     "Integrated")
         wlr-randr --output eDP-1 --on
